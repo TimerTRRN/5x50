48fd957595886ae078fa02929bba5a7ac80a1e81
5x50 <2032wkhiev@johnstonschools.org>
2026-09-25 12:11:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ var APPS = {
     'cine': {title: 'Youtube', path: 'script/Apps/Cine/index.html', icon: 'https://cdn.phototourl.com/member/2026-09-25-3c4a1e92-b357-47f4-80fc-7d0cc933285c.png', pinned: true},
     'term': {title: 'Spotify', path: 'script/Apps/Spotify/index.html', icon: 'https://cdn.pixabay.com/photo/2016/10/22/00/15/spotify-1759471_1280.jpg', pinned: true},
     'files': {title: 'CINE-PLAY', path: 'script/Apps/Ps5/index.html', icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ-OeL_be7RFaoHi3PswkuAR5XcMgBNRDynsg&s', pinned: true},
-    'web': {title: 'TRRN-Web', path: 'script/Apps/Web/index.html', icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSeD89ZcX5W1FBtal7RerasT27q-OmZqnBixQ&s', pinned: true},
+    'web': {title: 'MSpaint', path: 'script/Apps/Web/index.html', icon: 'https://cdn.phototourl.com/member/2026-09-25-f1153bae-c4f1-4868-94d0-afadf4bca917.png', pinned: true},
     'settings': {title: 'Settings', internal: true, icon: 'https://cdn.phototourl.com/member/2026-09-25-77b6be6d-96fd-4443-aa86-87e445ca7471.png', pinned: true},
     'discord': {title: 'Discord', path: 'script/Apps/Discord/index.html', icon: 'https://assets-global.website-files.com/6257adef93867e50d84d30e2/636e0a6a49cf127bf92de1e2_icon_clyde_blurple_RGB.png', pinned: false},
     'roblox': {title: 'Roblox', path: 'script/Apps/Roblox/index.html', icon: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9KvNyFWMg_bjo_q_1IVLKFWbfCeonn2qDow&s', pinned: false},
